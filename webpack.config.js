e550fecd516984a99b050792b9e54ac9441d82c2
maguireong <maguire2622@yahoo.com.sg>
2024-11-22 09:48:32 +0800

    Update json configs

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const webpack = require("webpack");
 module.exports = {
   entry: "./src/index.tsx", // Entry point of your app
   output: {
-    filename: "static/js/bundle.js", // JavaScript bundle
+    filename: "static/js/[name].[contenthash].bundle.js", // Cache-busting filenames
     path: path.resolve(__dirname, "build"), // Output directory
     publicPath: "/", // Ensures correct routing for SPAs
   },
@@ -28,12 +28,16 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/, // Process image files
         type: "asset/resource",
+        generator: {
+          filename: "static/media/[name][ext][query]", // Ensure assets go into static/media
+        },
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./public/index.html", // Path to your source HTML file
+      inject: "body", // Inject the script tag for bundle.js at the end of the body
     }),
     new NodePolyfillPlugin(), // For polyfilling Node modules
     new webpack.ProvidePlugin({
